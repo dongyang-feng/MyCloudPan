f53f635b2d5b3bd65998b33d47e46e54ddaea21f
fdy <1484231731@qq.com>
2023-06-19 14:25:34 +0800

    add a.c b.c

--- fdy.cpp
+++ fdy.cpp
@@ -1,1 +1,17 @@
+int printf(const char* format, ...);
+int global_init_var = 84;
+int global_uninit_var;
 
+void func( int i)
+{
+    printf("%d\n", i);
+}
+int main()
+{
+    static int static_var = 85;
+    static int static_var2;
+    int a = 1;
+    int b;
+    func(static_var + static_var2 + a + b);
+    return a;
+}
